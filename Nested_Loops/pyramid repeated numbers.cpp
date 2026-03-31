424e69bd41c1352d6f1fdd0e5906fc3b849347e6
Tazmeen Zahra <sofiasareem07@gmail.com>
2026-03-31 14:16:58 +0500

    Create pyramid repeated numbers.cpp

---
+++ Nested_Loops/pyramid repeated numbers.cpp
@@ -0,0 +1,17 @@
+//Pyramid repeated pattern of numbers using for loop
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Input number of rows: ";
+    cin >> n;
+    for (int i = 1; i <= n; i ++)
+    {
+        for (int j = 1; j <= i; j ++)
+        {
+            cout << i;
+        }
+        cout << endl;
+    }
+    return 0;
+}
