e1ffd150c4f8b21ebf74047abb659cea13f140b2
Tazmeen Zahra <sofiasareem07@gmail.com>
2026-03-31 14:03:14 +0500

    Create print numbers using for loop.cpp

---
+++ For_Loops/print numbers using for loop.cpp
@@ -0,0 +1,9 @@
+//Printing numbers from 1 to 5 through for loop
+#include <iostream>
+using namespace std;
+int main() {
+    for (int num1 = 1; num1 <=5; num1 ++) {
+        cout << num1 << endl;
+    }
+    return 0;
+}
