dad8a8428266abdc16088aadf58abc65c40f3b39
Tazmeen Zahra <sofiasareem07@gmail.com>
2026-03-31 14:06:09 +0500

    Create reverse 1-10.cpp

---
+++ For_Loops/reverse 1-10.cpp
@@ -0,0 +1,10 @@
+// Print numbers 10 to 1 in reverse order through for loop
+#include <iostream>
+using namespace std;
+int main() {
+    int num;
+    for (int num = 10; num >= 1; num --) {
+        cout << num << '\t';
+    }
+    return 0;
+}
